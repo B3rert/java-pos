724fe1d597f42e50e9c4ffbee27585ec93c202e5
Robert Morales <lsurec@demosoft.com.gt>
2023-11-09 21:30:32 -0600

    addd cors

--- src/main/java/com/example/demo/controllers/RegistroController.java
+++ src/main/java/com/example/demo/controllers/RegistroController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class RegistroController {
 
     @PersistenceContext
--- src/main/java/com/example/demo/controllers/TransaccionController.java
+++ src/main/java/com/example/demo/controllers/TransaccionController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("/api/transacciones")
+@CrossOrigin(origins = "*")
 public class TransaccionController {
 
     @Autowired
